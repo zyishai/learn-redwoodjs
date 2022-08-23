633753fdf143c93b71a77bbab70a59bcdfd72409
Yishai Zehavi <yishaizehavi@Yishais-MacBook-Pro-M1.local>
2022-08-23 21:27:12 +0300

    finished chapter 4

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,28 @@
+import Article from 'src/components/Article';
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ articles }) => {
+  return (
+    <ul>
+      {articles.map((item) => <Article key={item.id} article={item} />)}
+    </ul>
+  )
+}
